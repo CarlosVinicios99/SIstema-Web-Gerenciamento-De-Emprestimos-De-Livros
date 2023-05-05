9cab145316cb7fc9fc083935cb317799dacddf03
Carlos Vinicios <carlosvinicios@id.uff.br>
2023-05-05 13:41:26 -0300

    criacao da api spring boot

---
+++ api/web_emprestimo_de_livros/src/main/java/com/example/web_emprestimo_de_livros/WebEmprestimoDeLivrosApplication.java
@@ -0,0 +1,13 @@
+package com.example.web_emprestimo_de_livros;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebEmprestimoDeLivrosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebEmprestimoDeLivrosApplication.class, args);
+	}
+
+}
---
+++ api/web_emprestimo_de_livros/src/test/java/com/example/web_emprestimo_de_livros/WebEmprestimoDeLivrosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.web_emprestimo_de_livros;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebEmprestimoDeLivrosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

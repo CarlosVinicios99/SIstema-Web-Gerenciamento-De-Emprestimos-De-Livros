8c4cc790638a929e4fbaf78bdd7d49b005088126
Carlos Vinicios <carlosvinicios@id.uff.br>
2023-05-08 01:02:36 -0300

    entidade emprestimo inicialmente implementada

--- api_emprestimo_de_livros/web_emprestimo_de_livros/src/main/java/com/example/web_emprestimo_de_livros/model/Emprestimo.java
+++ api_emprestimo_de_livros/web_emprestimo_de_livros/src/main/java/com/example/web_emprestimo_de_livros/model/Emprestimo.java
@@ -1,5 +1,61 @@
 package com.example.web_emprestimo_de_livros.model;
 
-public class Emprestimo {
+import java.util.Date;
 
+public class Emprestimo {
+	
+	private Long id;
+	
+	private Livro livro;
+	
+	private Date dataInicial;
+	
+	private Date dataFinal;
+	
+	public Emprestimo() {
+		
+	}
+	
+	public Emprestimo(Long id, Livro livro, Date dataInicial, Date dataFinal) {
+		super();
+		this.id = id;
+		this.livro = livro;
+		this.dataInicial = dataInicial;
+		this.dataFinal = dataFinal;
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public Livro getLivro() {
+		return livro;
+	}
+
+	public void setLivro(Livro livro) {
+		this.livro = livro;
+	}
+
+	public Date getDataInicial() {
+		return dataInicial;
+	}
+
+	public void setDataInicial(Date dataInicial) {
+		this.dataInicial = dataInicial;
+	}
+
+	public Date getDataFinal() {
+		return dataFinal;
+	}
+
+	public void setDataFinal(Date dataFinal) {
+		this.dataFinal = dataFinal;
+	}
+	
+	
+	
 }

485044820e4ad26fcf7efd79d82fbe105287d553
Carlos Vinicios <carlosvinicios@id.uff.br>
2023-06-08 16:18:51 -0300

    Controller para obter Usuario criado

--- api_emprestimo_de_livros/web_emprestimo_de_livros/src/main/java/com/example/web_emprestimo_de_livros/controller/UsuarioController.java
+++ api_emprestimo_de_livros/web_emprestimo_de_livros/src/main/java/com/example/web_emprestimo_de_livros/controller/UsuarioController.java
@@ -1,5 +1,30 @@
 package com.example.web_emprestimo_de_livros.controller;
 
-public class UsuarioController {
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 
+import com.example.web_emprestimo_de_livros.model.Bibliotecario;
+import com.example.web_emprestimo_de_livros.model.Usuario;
+import com.example.web_emprestimo_de_livros.service.BibliotecarioService;
+
+@RestController
+@RequestMapping("/usuario")
+@CrossOrigin
+public class UsuarioController {
+	
+	@Autowired
+	private UsuarioService usuarioService;
+	
+	
+	@GetMapping
+	public ResponseEntity<Usuario> obterUsuario(@RequestParam String cpf, @RequestParam String senha) {
+		Bibliotecario obj = usuarioService.obterUsuario(cpf, senha);
+		return ResponseEntity.ok().body(obj);		
+	}
+	
 }

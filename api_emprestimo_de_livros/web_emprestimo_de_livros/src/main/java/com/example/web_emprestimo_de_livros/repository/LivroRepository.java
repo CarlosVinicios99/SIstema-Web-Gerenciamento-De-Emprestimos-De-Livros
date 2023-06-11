e925eaa05df2d62143b698a1f846ade4d4b57c06
Carlos Vinicios <carlosvinicios@id.uff.br>
2023-06-10 22:35:53 -0300

    criacao de repositorio e servico para livro

---
+++ api_emprestimo_de_livros/web_emprestimo_de_livros/src/main/java/com/example/web_emprestimo_de_livros/repository/LivroRepository.java
@@ -0,0 +1,10 @@
+package com.example.web_emprestimo_de_livros.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.example.web_emprestimo_de_livros.model.Livro;
+
+public interface LivroRepository extends JpaRepository<Livro, Long>{
+	
+	
+}
---
+++ api_emprestimo_de_livros/web_emprestimo_de_livros/src/main/java/com/example/web_emprestimo_de_livros/service/LivroService.java
@@ -0,0 +1,20 @@
+package com.example.web_emprestimo_de_livros.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.example.web_emprestimo_de_livros.model.Livro;
+import com.example.web_emprestimo_de_livros.repository.LivroRepository;
+
+@Service
+public class LivroService {
+	
+	@Autowired
+	private LivroRepository livroRepository;
+	
+	public Livro criarLivro(Livro livro) {
+		return livroRepository.save(livro);
+	}
+	
+	
+}

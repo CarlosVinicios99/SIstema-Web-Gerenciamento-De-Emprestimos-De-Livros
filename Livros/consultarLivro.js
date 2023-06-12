826327fed0542005cc904e8f9ab03a6895eed338
Carlos Vinicios <carlosvinicios@id.uff.br>
2023-06-12 19:58:35 -0300

    script para pagina de consulta de livro

---
+++ Livros/consultarLivro.js
@@ -0,0 +1,30 @@
+(function(){
+
+    const botaoConsultar = document.querySelector("#botaoConsultar")
+    botaoConsultar.addEventListener("click", (event) => {
+        event.preventDefault()
+        buscarLivro()
+    })
+})
+
+async function buscarLivros(){
+    const titulo = document.querySelector("#inputTitulo").value
+
+    const url = `http://localhost:8080/livro/${titulo}`
+    try{
+        const response = await fetch(url)
+        const livros = await response.json()
+        console.log(livros)
+
+        if(livros != null){
+      //      window.alert(`Tem certeza que deseja excluir o livro ${livro.titulo}`)
+        }
+        else{
+            window.alert(`Livro nao existe!`)
+        }
+
+    }
+    catch(error){
+        console.log(`Erro ao buscar Livro: ${error}`)
+    }
+}

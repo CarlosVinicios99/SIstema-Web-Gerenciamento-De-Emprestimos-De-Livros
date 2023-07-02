7f50c5a9803ba92adbd3731fcd35928a41b551d3
Carlos Vinicios <carlosvinicios@id.uff.br>
2023-07-02 14:41:27 -0300

    exibicao dos emprestimos de usuario implementada

--- Emprestimos/meusEmprestimos.js
+++ Emprestimos/meusEmprestimos.js
@@ -1,7 +1,7 @@
 (function(){
     const botaoConfirmar = document.querySelector("#botaoConfirmar")
 
-    botaoAvancar.addEventListener("click", (event) => {
+    botaoConfirmar.addEventListener("click", (event) => {
         event.preventDefault()
         obterMeusEmprestimos()
     })
@@ -16,13 +16,14 @@ async function obterMeusEmprestimos(){
     }
 
     else{
-        let url = `http://localhost:8080/usuario?cpf=${cpf}`
+        let url = `http://localhost:8080/usuario/cpf?cpf=${cpf}`
         let usuario
         try{
             let response = await fetch(url)
             if(response.ok) {
                 usuario = await response.json();
-                console.log(usuario)
+                console.log(usuario.emprestimos)
+                exibirMeusEmprestimos(usuario.emprestimos)
             }
             else{
                 window.alert("Os dados nao correspondem a nenhum registro!\nTente Novamente!")
@@ -32,24 +33,6 @@ async function obterMeusEmprestimos(){
         catch(error){
             console.log(`Erro ao buscar usuario: ${error}`)
         }
-
-        url = `http://localhost:8080/livro/consulta?titulo=${titulo}`
-        try{
-            response = await fetch(url)
-            if(response.ok) {
-                const livros = await response.json();
-                console.log(livros)
-                limparBuscaAnterior()
-                exibirLivrosParaAdicionarEmprestimos(livros, usuario)
-            }
-            else{
-                window.alert("Os dados nao correspondem a nenhum registro!\nTente Novamente!")
-                return
-            }
-        }
-        catch(error){
-            console.log(`Erro ao buscar livro: ${error}`)
-        }
     }
 }
 
@@ -57,14 +40,15 @@ function obterInputsDeEntrada(){
     const cpf = document.querySelector("#inputCPF").value
     return cpf
 }
-function validarDadosDeEntrada(cpf, senha, livro){
+
+function validarDadosDeEntrada(cpf){
     if(cpf === ""){
         return false
     }
     return true
 }
 
-async function exibirLivrosParaAdicionarEmprestimos(livros, usuario){
+async function exibirMeusEmprestimos(emprestimos){
 
     const resultadoDaConsultaContainer = document.querySelector("#resultadoDaConsulta")
     const tituloResultados = document.createElement("h2")
@@ -72,65 +56,42 @@ async function exibirLivrosParaAdicionarEmprestimos(livros, usuario){
     tituloResultados.innerText = "Resultados"
     resultadoDaConsultaContainer.append(tituloResultados)   
 
-    for(let i = 0; i < livros.length; i++){
+    for(let i = 0; i < emprestimos.length; i++){
         if(i === 0){
-            const divLivro = document.createElement("div")
+            const divEmprestimo = document.createElement("div")
             const titulo = document.createElement("span")
             titulo.innerText = ""
             titulo.classList.add("titulo-livro")
-            divLivro.appendChild(titulo)
-            divLivro.classList.add("livro-container")
-            resultadoDaConsultaContainer.appendChild(divLivro)
-        }
-        const divLivro = document.createElement("div")
-        const titulo = document.createElement("span")
-        const autor = document.createElement("span")
-        const status = document.createElement("span")
-        const proprietario = document.createElement("span")
-        const contato = document.createElement("span")
-        const botaoEmprestimo = document.createElement("button")
-
-
-        titulo.innerText = "TITULO: " + livros[i].titulo
-        autor.innerText = "AUTOR: " + livros[i].autor
-        status.innerText = livros[i].disponibilidade ? "DISPONÍVEL": "INDISPONÍVEL"
-        proprietario.innerText =  "PROPRIETÁRIO: " + livros[i].proprietario
-        botaoEmprestimo.innerText = "Emprestar"
-        
-
-        titulo.classList.add("titulo-livro")
-        autor.classList.add("titulo-livro")  
-        proprietario.classList.add("titulo-livro")
-        contato.classList.add("titulo-livro")
-        botaoEmprestimo.classList.add("botao-emprestimo")
-
-        botaoEmprestimo.addEventListener("click", (event) => {
-            event.preventDefault()
-            realizarEmprestimo(usuario, livros[i])
-        })
-
-        if(livros[i].disponibilidade){
-            status.classList.add("status-disponibilidade")
-        }
-        else{
-            status.classList.add("status-indisponibilidade")
+            divEmprestimo.appendChild(titulo)
+            divEmprestimo.classList.add("livro-container")
+            resultadoDaConsultaContainer.appendChild(divEmprestimo)
         }
 
-        divLivro.appendChild(titulo)
-        divLivro.appendChild(autor)
-        divLivro.appendChild(proprietario)
-        divLivro.appendChild(status)
+        const divEmprestimo = document.createElement("div")
+        const tituloLivro = document.createElement("span")
+        const dataInicial = document.createElement("span")
+        const dataFinal = document.createElement("span")
 
-        if(livros[i].proprietario !== "biblioteca"){
-            contato.innerText = "CONTATO: " + livros[i].proprietario.replace(" ", "").toLowerCase() + "@email.com"
-            divLivro.append(contato)
-        }
+        const dataInicialConteudo = emprestimos[i].dataInicial
+        const dataInicialFormatada = new Date(dataInicialConteudo).toLocaleDateString("pt-BR")
 
-        if(livros[i].proprietario === "biblioteca" && livros[i].disponibilidade){
-            divLivro.appendChild(botaoEmprestimo)
-        }
-       
-        divLivro.classList.add("livro-container")
-        resultadoDaConsultaContainer.appendChild(divLivro)
+        const dataFinalConteudo = emprestimos[i].dataFinal
+        const dataFinalFormatada = new Date(dataFinalConteudo).toLocaleDateString("pt-BR")
+
+        tituloLivro.innerText = "LIVRO: " + emprestimos[i].livro.titulo
+        dataInicial.innerText = "DATA INICIAL: " + dataInicialFormatada
+        dataFinal.innerText = "DATA FINAL: " + dataFinalFormatada
+    
+        tituloLivro.classList.add("titulo-livro")
+        dataInicial.classList.add("titulo-livro")
+        dataFinal.classList.add("titulo-livro")
+      
+        divEmprestimo.appendChild(tituloLivro)
+        divEmprestimo.appendChild(dataInicial)
+        divEmprestimo.appendChild(dataFinal)
+
+        divEmprestimo.classList.add("livro-container")
+        resultadoDaConsultaContainer.appendChild(divEmprestimo) 
+
     }
 }
